1373c133a73da0b10ebe7ad66dd8091882328c75
Irrryna <125253721+Irrryna@users.noreply.github.com>
2025-04-28 16:35:51 +0200

    up

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 for ($i=0; $i < 0; $i++) {
     echo "Hello World!<br>";
 }
-$test = ["1, 2, 3, 4, 5"]
+$test = ["1, 2, 3, 4, 5"];
 $total = 0;
 foreach ($test as $key => $value) {
     $total += $value + $total;
@@ -18,7 +18,7 @@ printf($total);
 
 function totalArray($array) {
     $total = 0;
-    for (index = 0; $index < count($array); $index++) {
+    for ($index = 0; $index < count($array); $index++) {
         $total += $array[$index];
     }
 }
